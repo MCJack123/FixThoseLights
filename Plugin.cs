e883aedb90630fb02bc276f415de648c40821b7e
MCJack123 <jackmacwindowslinux@gmail.com>
2021-03-10 22:29:25 -0500

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,95 @@
+﻿using IPA;
+using IPA.Config;
+using IPA.Config.Stores;
+using System;
+using System.Reflection;
+using IPALogger = IPA.Logging.Logger;
+
+namespace FixThoseLights {
+    [Plugin(RuntimeOptions.DynamicInit)]
+    public class Plugin {
+        public const string HarmonyId = "com.github.jackmacwindows.FixThoseLights";
+        internal static readonly HarmonyLib.Harmony harmony = new HarmonyLib.Harmony(HarmonyId);
+
+        internal static Plugin Instance { get; private set; }
+        internal static IPALogger Log { get; private set; }
+
+        [Init]
+        /// <summary>
+        /// Called when the plugin is first loaded by IPA (either when the game starts or when the plugin is enabled if it starts disabled).
+        /// [Init] methods that use a Constructor or called before regular methods like InitWithConfig.
+        /// Only use [Init] with one Constructor.
+        /// </summary>
+        public Plugin(IPALogger logger) {
+            Instance = this;
+            Plugin.Log = logger;
+            Plugin.Log?.Debug("Logger initialized.");
+        }
+
+        #region BSIPA Config
+        [Init]
+        public void InitWithConfig(Config conf)
+        {
+            Configuration.PluginConfig.Instance = conf.Generated<Configuration.PluginConfig>();
+            Plugin.Log?.Debug("Config loaded");
+        }
+        #endregion
+
+
+        #region Disableable
+        /// <summary>
+        /// Called when the plugin is enabled (including when the game starts if the plugin is enabled).
+        /// </summary>
+        [OnEnable]
+        public void OnEnable() {
+            ApplyHarmonyPatches();
+        }
+
+        /// <summary>
+        /// Called when the plugin is disabled and on Beat Saber quit. It is important to clean up any Harmony patches, GameObjects, and Monobehaviours here.
+        /// The game should be left in a state as if the plugin was never started.
+        /// Methods marked [OnDisable] must return void or Task.
+        /// </summary>
+        [OnDisable]
+        public void OnDisable() {
+            RemoveHarmonyPatches();
+        }
+        #endregion
+
+        #region Harmony
+        /// <summary>
+        /// Attempts to apply all the Harmony patches in this assembly.
+        /// </summary>
+        internal static void ApplyHarmonyPatches()
+        {
+            try
+            {
+                Plugin.Log?.Debug("Applying Harmony patches.");
+                harmony.PatchAll(Assembly.GetExecutingAssembly());
+            }
+            catch (Exception ex)
+            {
+                Plugin.Log?.Error("Error applying Harmony patches: " + ex.Message);
+                Plugin.Log?.Debug(ex);
+            }
+        }
+
+        /// <summary>
+        /// Attempts to remove all the Harmony patches that used our HarmonyId.
+        /// </summary>
+        internal static void RemoveHarmonyPatches()
+        {
+            try
+            {
+                // Removes all patches with this HarmonyId
+                harmony.UnpatchAll(HarmonyId);
+            }
+            catch (Exception ex)
+            {
+                Plugin.Log?.Error("Error removing Harmony patches: " + ex.Message);
+                Plugin.Log?.Debug(ex);
+            }
+        }
+        #endregion
+    }
+}
