513bb25bb992a82fc1d86e0850a4cfd256a42375
MCJack123 <jackmacwindowslinux@gmail.com>
2021-03-10 22:48:23 -0500

    Consolidated files, added readme

--- Plugin.cs
+++ Plugin.cs
@@ -3,6 +3,8 @@ using IPA.Config;
 using IPA.Config.Stores;
 using System;
 using System.Reflection;
+using HarmonyLib;
+using UnityEngine;
 using IPALogger = IPA.Logging.Logger;
 
 namespace FixThoseLights {
@@ -91,5 +93,22 @@ namespace FixThoseLights {
             }
         }
         #endregion
+    }
+
+    [HarmonyPatch(typeof(FlickeringNeonSign), "OnEnable")]
+    class Patch {
+        static AccessTools.FieldRef<FlickeringNeonSign, RandomObjectPicker<AudioClip>> clipPickerRef = AccessTools.FieldRefAccess<FlickeringNeonSign, RandomObjectPicker<AudioClip>>("_sparksAudioClipPicker");
+        static AccessTools.FieldRef<FlickeringNeonSign, AudioClip[]> clipsRef = AccessTools.FieldRefAccess<FlickeringNeonSign, AudioClip[]>("_sparksAudioClips");
+        static bool Prefix(FlickeringNeonSign __instance) {
+            clipPickerRef(__instance) = new CustomAudioPicker(clipsRef(__instance), 0.1f);
+            return !(Configuration.PluginConfig.Instance.enabled && Configuration.PluginConfig.Instance.disableFlicker);
+        }
+    }
+
+    internal class CustomAudioPicker : RandomObjectPicker<AudioClip> {
+        public CustomAudioPicker(AudioClip[] clips, float interval): base(clips, interval) {}
+        public override AudioClip PickRandomObject() {
+            return Configuration.PluginConfig.Instance.enabled ? null : base.PickRandomObject();
+        }
     }
 }
---
+++ README.md
@@ -0,0 +1,15 @@
+﻿# FixThoseLights
+Fixes the blinking E in the Beat Saber logo with a light tap. Or just mutes it. 
+
+## Requirements
+* Beat Saber 1.13.4
+* BSIPA 4.1.4
+
+## Usage
+Just drop the DLL into the Plugins folder.
+
+## Config
+The configuration file is at UserData/FixThoseLights.json, and contains the following values:
+
+* `enabled`: Whether to enable the mod (disables sounds). Defaults to `true`.
+* `disableFlicker`: Whether to disable the flickering E letter. Defaults to `false`.
